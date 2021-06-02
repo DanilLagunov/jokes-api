030bea28528760c127fd90e145069b7380c400c1
DanilLagunov <raccoo1803@gmail.com>
2021-06-02 17:42:19 +0300

    Second commit

--- main.go
+++ main.go
@@ -1,9 +1,176 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
+
 	"github.com/gorilla/mux"
 )
 
+// Joke struct with constructor
+
+type Joke struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+	Score int    `json:"score"`
+}
+
+func NewJoke(id, title, body string, score int) Joke {
+	return Joke{id, title, body, score}
+}
+
+var jokes []Joke
+
+// HANDLERS
+
+func getJokes(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", jokes)
+}
+
+func addJoke(w http.ResponseWriter, r *http.Request) {
+	// Generating ID and taking data from form
+	var id string
+	title := r.FormValue("title")
+	body := r.FormValue("body")
+	score := 0
+
+	// Check for uniqueness
+CHECK:
+	id = GenerateId()
+	for i := 0; i < len(jokes); i++ {
+		if id == jokes[i].ID {
+			goto CHECK
+		}
+	}
+
+	// Creating new joke
+	joke := NewJoke(id, title, body, score)
+	jokes = append(jokes, joke)
+
+	//Formatting and adding joke to the file
+	rawDataOut, err := json.MarshalIndent(&jokes, "", "   ")
+	if err != nil {
+		log.Fatal("JSON marshaling failed: ", err)
+	}
+	err = ioutil.WriteFile("reddit_jokes.json", rawDataOut, 0)
+	if err != nil {
+		log.Fatal("Cannot write:", err)
+	}
+
+	//Redirecting to main page
+	http.Redirect(w, r, "/jokes", 302)
+}
+
+func getJoke(w http.ResponseWriter, r *http.Request) {
+	//Taking search values from URL
+	text := r.URL.Query().Get("text")
+	id := r.URL.Query().Get("id")
+
+	var result []Joke
+	t, err := template.ParseFiles("templates/findjoke.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	//Searching by text
+	if text != "" {
+		for _, item := range jokes {
+			if strings.Contains(item.Title, text) || strings.Contains(item.Body, text) {
+				result = append(result, item)
+			}
+		}
+		t.ExecuteTemplate(w, "findjoke", result)
+	}
+
+	//Searching by ID
+	if id != "" {
+		for _, item := range jokes {
+			if item.ID == id {
+				result = append(result, item)
+				t.ExecuteTemplate(w, "findjoke", result)
+			}
+		}
+	}
+}
+
+func getRandomJokes(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/random.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	s := rand.NewSource(time.Now().UnixNano())
+	rnd := rand.New(s)
+
+	//Filling an array with random elements
+	var rndjokes [100]Joke
+	for i := 0; i < 100; i++ {
+		rndjokes[i] = jokes[rnd.Intn(len(jokes))]
+	}
+
+	t.ExecuteTemplate(w, "random", rndjokes)
+}
+
+func getFunniestJokes(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/funniest.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	//Sorting an array by score
+	funniest := jokes
+	sort.Slice(funniest, func(i, j int) (less bool) {
+		return funniest[i].Score > funniest[j].Score
+	})
+
+	t.ExecuteTemplate(w, "funniest", funniest[0:99])
+}
+
+// ADDITIONAL FUNCS
+
+func parseJSON(path string, list *[]Joke) {
+	file, _ := os.Open(path)
+	decoder := json.NewDecoder(file)
+
+	err := decoder.Decode(&list)
+	if err != nil {
+		fmt.Println("Decode error")
+	}
+}
+
+func GenerateId() string {
+	b := make([]byte, 3)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+// MAIN FUNC
+
 func main() {
-	router := mux.NewRouter()
+	parseJSON("reddit_jokes.json", &jokes)
+
+	r := mux.NewRouter()
+
+	r.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
+	r.HandleFunc("/jokes", getJokes).Methods("GET")
+	r.HandleFunc("/jokes/add", addJoke).Methods("POST")
+	r.HandleFunc("/jokes/search", getJoke).Methods("GET")
+	r.HandleFunc("/jokes/random", getRandomJokes).Methods("GET")
+	r.HandleFunc("/jokes/funniest", getFunniestJokes).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
