128a8a9effab6e31e91a9df667eaa56a744304ae
DanilLagunov <raccoo1803@gmail.com>
2021-12-30 22:43:06 +0200

    fix db connection problem.

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	storage, err := mongodb.NewDatabase("mongodb://mongo-dev:27017")
+	storage, err := mongodb.NewDatabase("mongodb://mongo:27017")
 	if err != nil {
 		log.Fatal(err)
 	}
