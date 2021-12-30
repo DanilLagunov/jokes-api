1e14d444ad62f4646db71f7b1666995f92b714c5
DanilLagunov <raccoo1803@gmail.com>
2021-12-30 22:17:40 +0200

    fix mongo container

--- pkg/storage/mongodb/mongodb.go
+++ pkg/storage/mongodb/mongodb.go
@@ -31,7 +31,7 @@ func NewDatabase() (*Database, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongo-dev:27017"))
 	db.client = client
 	collection := client.Database(DBName).Collection(JokesCollectionName)
 	db.jokesCollection = collection
